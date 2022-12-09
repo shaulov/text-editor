f7321d96256d0d0c22e793e6771b380705f182b2
shaulov <shineginn@gmail.com>
2022-12-09 12:31:39 +0300

    Add variable with current block type with which can add active state

--- src/hooks/use-editor.tsx
+++ src/hooks/use-editor.tsx
@@ -6,6 +6,7 @@ export type EditorApi = {
   state: EditorState;
   onChange: (state: EditorState) => void;
   toggleBlockType: (blockType: BlockType) => void;
+  currentBlockType: BlockType;
 }
 
 export function useEditor(html?: string): EditorApi {
@@ -13,10 +14,18 @@ export function useEditor(html?: string): EditorApi {
   const toggleBlockType = useCallback((blockType: BlockType) => {
     setState((currentState) => RichUtils.toggleBlockType(currentState, blockType));
   }, []);
+  const currentBlockType = useMemo(() => {
+    const selection = state.getSelection();
+    const content = state.getCurrentContent();
+    const block = content.getBlockForKey(selection.getStartKey());
+
+    return block.getType() as BlockType;
+  }, [state]);
 
   return useMemo(() => ({
     state,
     onChange: setState,
     toggleBlockType,
-  }), [state, toggleBlockType]);
+    currentBlockType,
+  }), [state, toggleBlockType, currentBlockType]);
 }
