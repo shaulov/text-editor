b8e1c77171c54aa27172b170bee450a4376e9eae
shaulov <shineginn@gmail.com>
2022-12-09 12:19:46 +0300

    Add const file and move enum BlockType there

--- src/components/text-editor/config.ts
+++ src/components/text-editor/config.ts
@@ -1,27 +1,6 @@
 import Immutable from 'immutable';
 import { DefaultDraftBlockRenderMap } from 'draft-js';
-
-export enum BlockType {
-  /* Заголовки */
-  h1 = 'header-one',
-  h2 = 'header-two',
-  h3 = 'header-three',
-  h4 = 'header-four',
-  h5 = 'header-five',
-  h6 = 'header-six',
-  /* Цитата */
-  blockquote = 'blockquote',
-  /* Блок с кодом */
-  code = 'code-block',
-  /* Список */
-  list = 'unordered-list-item',
-  /* Нумерованный список */
-  orderList = 'ordered-list-item',
-  /* Сноска */
-  cite = 'cite',
-  /* Простой текст */
-  default = 'unstyled',
-}
+import { BlockType } from '../../const';
 
 const CUSTOM_BLOCK_RENDER_MAP = Immutable.Map({
   [BlockType.cite]: {
