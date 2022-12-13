8df94834c45d849a54c9f346915ac8920bce3817
shaulov <shineginn@gmail.com>
2022-12-13 20:31:14 +0300

    Take out onMouseDown function

--- src/components/tool-panel/tool-panel.tsx
+++ src/components/tool-panel/tool-panel.tsx
@@ -1,4 +1,5 @@
 // import { EditorApi } from '../../hooks/use-editor';
+import { MouseEvent } from 'react';
 import cn from 'classnames';
 import { useEditorApi } from '../../hooks/use-editor-context';
 import { INLINE_STYLE_CODES } from '../../const';
@@ -12,21 +13,24 @@ function ToolPanel({ className }: ToolPanelProps): JSX.Element {
   const { toggleInlineStyle, hasInlineStyle } = useEditorApi();
   return (
     <div className={cn('tool-panel', className)}>
-      {INLINE_STYLE_CODES.map((code) => (
-        <button
-          key={code}
-          className={cn(
-            'tool-panel__item',
-            hasInlineStyle(code) && 'tool-panel__item_active'
-          )}
-          onMouseDown={(evt) => {
-            evt.preventDefault();
-            toggleInlineStyle(code);
-          }}
-        >
-          {code}
-        </button>
-      ))}
+      {INLINE_STYLE_CODES.map((code) => {
+        const onMouseDown = (evt: MouseEvent<HTMLButtonElement>): void => {
+          evt.preventDefault();
+          toggleInlineStyle(code);
+        };
+        return (
+          <button
+            key={code}
+            className={cn(
+              'tool-panel__item',
+              hasInlineStyle(code) && 'tool-panel__item_active'
+            )}
+            onMouseDown={onMouseDown}
+          >
+            {code}
+          </button>
+        );
+      })}
     </div>
   );
 }
