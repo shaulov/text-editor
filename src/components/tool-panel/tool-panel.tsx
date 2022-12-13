8b2b5ff685bfe8a06fba3116490bc2a3a5993f35
shaulov <shineginn@gmail.com>
2022-12-13 20:51:14 +0300

    Add buttons for block type changing

--- src/components/tool-panel/tool-panel.tsx
+++ src/components/tool-panel/tool-panel.tsx
@@ -2,7 +2,7 @@
 import { MouseEvent } from 'react';
 import cn from 'classnames';
 import { useEditorApi } from '../../hooks/use-editor-context';
-import { INLINE_STYLE_CODES } from '../../const';
+import { BlockType, INLINE_STYLE_CODES } from '../../const';
 import './tool-panel.scss';
 
 type ToolPanelProps = {
@@ -10,9 +10,57 @@ type ToolPanelProps = {
 }
 
 function ToolPanel({ className }: ToolPanelProps): JSX.Element {
-  const { toggleInlineStyle, hasInlineStyle } = useEditorApi();
+  const { toggleBlockType, currentBlockType, toggleInlineStyle, hasInlineStyle } = useEditorApi();
   return (
     <div className={cn('tool-panel', className)}>
+      <button
+        className={cn(
+          'tool-panel__item',
+          currentBlockType === BlockType.h1 && 'tool-panel__item_active'
+        )}
+        onMouseDown={(evt) => {
+          evt.preventDefault();
+          toggleBlockType(BlockType.h1);
+        }}
+      >
+        Заголовок
+      </button>
+      <button
+        className={cn(
+          'tool-panel__item',
+          currentBlockType === BlockType.h2 && 'tool-panel__item_active'
+        )}
+        onMouseDown={(evt) => {
+          evt.preventDefault();
+          toggleBlockType(BlockType.h2);
+        }}
+      >
+        Подзаголовок
+      </button>
+      <button
+        className={cn(
+          'tool-panel__item',
+          currentBlockType === BlockType.cite && 'tool-panel__item_active'
+        )}
+        onMouseDown={(evt) => {
+          evt.preventDefault();
+          toggleBlockType(BlockType.cite);
+        }}
+      >
+        Сноска
+      </button>
+      <button
+        className={cn(
+          'tool-panel__item',
+          currentBlockType === BlockType.default && 'tool-panel__item_active'
+        )}
+        onMouseDown={(evt) => {
+          evt.preventDefault();
+          toggleBlockType(BlockType.default);
+        }}
+      >
+        Простой
+      </button>
       {INLINE_STYLE_CODES.map((code) => {
         const onMouseDown = (evt: MouseEvent<HTMLButtonElement>): void => {
           evt.preventDefault();
