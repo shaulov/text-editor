091c430da2b2ff8efb6e6f44838a7a2c98821c88
shaulov <shineginn@gmail.com>
2022-12-19 18:53:22 +0300

    Add function for convering HTML to editor state

--- src/utils/convert.tsx
+++ src/utils/convert.tsx
@@ -1,4 +1,4 @@
-import { convertToHTML } from 'draft-convert';
+import { convertToHTML, convertFromHTML } from 'draft-convert';
 import { CUSTOM_STYLE_MAP } from '../components/text-editor/config';
 import { BlockType, EntityType, InlineStyles } from '../const';
 
@@ -62,5 +62,63 @@ export const stateToHTML = convertToHTML<InlineStyles, BlockType>({
       /* eslint-enable*/
     }
     return originalText;
+  },
+});
+
+export const HTMLtoState = convertFromHTML<DOMStringMap, BlockType>({
+  htmlToStyle: (nodeName, node, currentStyle) => {
+    if (nodeName === 'strong') {
+      return currentStyle.add(InlineStyles.BOLD);
+    }
+
+    if (nodeName === 'em') {
+      return currentStyle.add(InlineStyles.ITALIC);
+    }
+
+    if (nodeName === 'span' && node.classList.contains('underline')) {
+      return currentStyle.add(InlineStyles.UNDERLINE);
+    }
+
+    if (nodeName === 'span' && node.classList.contains('accent')) {
+      return currentStyle.add(InlineStyles.ACCENT);
+    }
+
+    return currentStyle;
+  },
+  /* eslint-disable*/
+  /** Типизация пакета не предусматривает параметр last, но он есть */
+  // @ts-ignore
+  htmlToBlock(nodeName, node, last) {
+    switch (nodeName) {
+      case 'h1':
+        return BlockType.h1;
+      case 'h2':
+        return BlockType.h2;
+      case 'h3':
+        return BlockType.h3;
+      case 'h4':
+        return BlockType.h4;
+      case 'li':
+        if (last === 'ol') {
+          return BlockType.orderList;
+        }
+        return BlockType.list;
+      case 'blockquote':
+        return BlockType.blockquote;
+      case 'cite':
+        return BlockType.cite;
+      case 'div':
+      case 'p':
+        return BlockType.default;
+      default:
+        return null;
+    }
+  },
+  htmlToEntity: (nodeName, node, createEntity) => {
+    if (nodeName === 'a' && node.href) {
+      return createEntity(EntityType.link, 'MUTABLE', { url: node.href });
+    }
+
+    return undefined;
   },
 });
